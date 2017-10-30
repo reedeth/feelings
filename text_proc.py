4e2e122b37ec1b718fd7fb866a3063b138b2881c
walshbr <bmw9t@virginia.edu>
2017-10-30 11:43:17 -0400

    Add file to remove line numbers

--- text_proc.py
+++ text_proc.py
@@ -1,15 +1,33 @@
-def plural(word):
-    if word.endswith('y'):
-        return word[:-1] + 'ies'
-    elif word[-1] in 'sx' or word [-2:] in ['sh', 'ch']:
-        return word + 'es'
-    elif word.endswith('an'):
-        return word[:-2] + 'en'
-    else:
-        return word + 's'
+def clean_numbers(text):
+    """Takes a text and gets rid of the line numbers"""
+    processed_text = []
+    for line in text:
+        clean_line = []
+        for char in line:
+            if not char.isdigit():
+                clean_line.append(char)
+        processed_text.append(''.join(clean_line))
+    return processed_text
 
-def unusual_words(text):
-    text_vocab = set(w.lower() for w in text if w.isalpha())
-    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
-    unusual = text_vocab - english_vocab
-    return sorted(unusual)
+
+def write_file(filename, text):
+    """take the processed lines and recombine them in a new file"""
+    with open(filename[:-4] + '_clean.txt', 'w') as fout:
+        for line in text:
+            fout.write(line)
+
+
+def main():
+    # get a file's text
+    filename = 'corpus/sabotage.txt'
+    # open the file and get the lines
+    with open(filename, 'r') as fin:
+        raw_text = fin.readlines()
+    # take the raw lines and clean the numbers
+    clean_text = clean_numbers(raw_text)
+    # recombine them and write to a new file
+    write_file(filename, clean_text)
+
+
+if __name__ == "__main__":
+    main()
