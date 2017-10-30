b51361eb27ad6c24e4b2c0d8fe5b7b77d4360fbd
Ethan Reed <ecr6nd@virginia.edu>
2017-10-30 11:16:44 -0400

    Initializing repo with git, updating ignore file

---
+++ test.py
@@ -0,0 +1,5 @@
+from nltk.corpus import brown
+news_text = brown.words(categories='news')
+editorial_text = brown.words(categories='editorial')
+reviews_text = brown.words(categories='reviews')
+religion_text = brown.words(categories='religion')
---
+++ text_proc.py
@@ -0,0 +1,15 @@
+def plural(word):
+    if word.endswith('y'):
+        return word[:-1] + 'ies'
+    elif word[-1] in 'sx' or word [-2:] in ['sh', 'ch']:
+        return word + 'es'
+    elif word.endswith('an'):
+        return word[:-2] + 'en'
+    else:
+        return word + 's'
+
+def unusual_words(text):
+    text_vocab = set(w.lower() for w in text if w.isalpha())
+    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+    unusual = text_vocab - english_vocab
+    return sorted(unusual)
