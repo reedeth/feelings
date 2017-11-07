193d1233655305ec99a85ac15701527fb10eab03
Ethan Reed <ecr6nd@virginia.edu>
2017-11-07 17:38:00 -0500

    added method to style.py that should remove lowercase everything and remove punctuation, with notes on new lines to remove empty spaces in the tokens

--- style.py
+++ style.py
@@ -1,4 +1,5 @@
 import nltk
+import string
 
 
 def get_raw_text(fn):
@@ -16,8 +17,15 @@ def tokenize(line_text):
 
 def lowercase(all_tokens):
     """Given the tokenized text, lowercase everything"""
-    new_tokens = [[item.lower() for item in line] for line in all_tokens]
+    new_tokens = [[item.lower() for item in each_token] for each_token in all_tokens]
     return new_tokens
+
+def no_punctuation(text):
+    new_text = [[''.join(c for c in s if c not in string.punctuation) for s in y] for y in text]
+    return new_text
+
+# def no_spaces(text):
+#     new_text = [[s for s in x if s] for x in text]
 
 def main():
     # get a filename
@@ -26,6 +34,7 @@ def main():
     # 1. tokenize
     # 2. lowercase
     # 3. Get rid of punctuation
+    # 4. get rid of white space
     # segment if you care (if you care about sentences,
     #   lines, stanzas, poems)
     # tag with parts of speech
@@ -35,7 +44,17 @@ def main():
     raw_text = get_raw_text(filename)
     tokens = tokenize(raw_text)
     lower_tokens = lowercase(tokens)
-    print(lower_tokens)
+    without_punct = no_punctuation(lower_tokens)
+    print(without_punct)
+    # without_spaces = no_spaces(without_punct)
+    # print(without_spaces)
+
+    # print(lower_tokens)
+
+    # fdist = nltk.FreqDist(lower_tokens)
+    # modals = ['can', 'could', 'may', 'might', 'must', 'will']
+    # for m in modals:
+    #     print (m + ':', fdist[m], end=' ')
 
 
 if __name__ == "__main__":
