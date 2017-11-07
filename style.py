719d143bf0d799c4895b58a7a15ba3784a820ebe
Ethan Reed <ecr6nd@virginia.edu>
2017-11-07 16:53:47 -0500

    Working on style.py-- I think I found away to lowercase all the tokens in the tokenized text, but might have messed the tokens up. We shall see!

--- style.py
+++ style.py
@@ -7,7 +7,6 @@ def get_raw_text(fn):
         raw_text = fin.readlines()
     return raw_text
 
-
 def tokenize(line_text):
     """Given raw text that is a list of lines, produce the tokens"""
     line_tokens = []
@@ -15,21 +14,28 @@ def tokenize(line_text):
         line_tokens.append(nltk.word_tokenize(line))
     return line_tokens
 
+def lowercase(all_tokens):
+    """Given the tokenized text, lowercase everything"""
+    new_tokens = [[item.lower() for item in line] for line in all_tokens]
+    return new_tokens
 
 def main():
     # get a filename
     # get raw text
     # preprocess that text:
-    # 1. tokenize, lowercase, get rid of punctuation if you want.
-    # 2. segment if you care (if you care about sentences,
+    # 1. tokenize
+    # 2. lowercase
+    # 3. Get rid of punctuation
+    # segment if you care (if you care about sentences,
     #   lines, stanzas, poems)
-    # 3. tag with parts of speech
+    # tag with parts of speech
     # do the stuff you actually care about
 
     filename = 'corpus/sabotage_clean.txt'
     raw_text = get_raw_text(filename)
     tokens = tokenize(raw_text)
-    print(tokens)
+    lower_tokens = lowercase(tokens)
+    print(lower_tokens)
 
 
 if __name__ == "__main__":
