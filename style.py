9d484dfda000c7eadc43dd863a9598aa6eb7a466
walshbr <bmw9t@virginia.edu>
2017-11-09 12:58:39 -0500

    Update style with sentiment analysis

--- style.py
+++ style.py
@@ -1,6 +1,9 @@
 import nltk
 import string
 from nltk.probability import FreqDist
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+from bokeh.plotting import figure, output_file, show
 
 class Text(object):
     def __init__(self, fn):
@@ -11,9 +14,43 @@ class Text(object):
         self.processed_tokens = self.lowercase()
         self.processed_tokens = self.no_punctuation()
         self.processed_tokens = self.no_spaces()
-        
+        self.flattened_tokens = self.flatten(self.processed_tokens)
+        self.stringified_text = self.get_stringified_text()
+        self.stringified_sentences = self.get_stringified_sentences()
+        self.sentiments = self.get_sentiment()
+        self.sentiment_values = self.get_sentiment_values()
+
+    # def bokeh_graph(self):
+    #     output_file('bokeh_test.html')
+    #     p = figure(plot_width=400, plot_height=400)
+    #     p.line(self.sentiment_values, line_width=2)
+    #     show(p)
+
+    def graph_sentiment(self):
+        plt.plot(self.sentiment_values)
+        plt.ylabel('polarity')
+        plt.xlabel('position in text')
+        plt.title('Sentiment Analysis of Text')
+        plt.show()
+
+    def get_sentiment_values(self):
+        return [val.polarity for val in self.sentiments]
+
+    def get_sentiment(self):
+        return [TextBlob(line).sentiment for line in self.stringified_sentences]
+
 
     # methods live here
+    def flatten(self, thing):
+        return [item for sublist in thing for item in sublist]
+
+    def get_stringified_sentences(self):
+        return [' '.join(line) for line in self.processed_tokens]
+
+
+    def get_stringified_text(self):
+        return ' '.join(self.flattened_tokens)
+
     def get_raw_text(self):
         """Given a filename, get the raw text"""
         with open(self.filename, 'r') as fin:
